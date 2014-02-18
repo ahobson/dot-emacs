989fa95a90ea3f84cf700809d634a700b70ced73
Andrew Hobson <ahobson@gmail.com>
2014-02-18 11:23:05 -0500

    Need paredit in cider

--- ahobson/my-clojure.el
+++ ahobson/my-clojure.el
@@ -1,5 +1,6 @@
 (add-hook 'nrepl-mode-hook 'subword-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'my-turn-on-paredit)
+(add-hook 'cider-repl-mode-hook 'my-turn-on-paredit)
 
 (provide 'my-clojure)
