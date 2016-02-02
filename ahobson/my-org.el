e192cdb2ab1378e51f3f1c0f68e914cd901bd2dd
Andrew Hobson <ahobson@damballa.com>
2016-02-02 10:22:07 -0500

    add org markdown export

--- ahobson/my-org.el
+++ ahobson/my-org.el
@@ -1,7 +1,5 @@
 (setq org-directory "~/Dropbox/org")
 (setq org-catch-invisible-edits 'smart)
 
-;; set up mobile org
-(require 'org-mobile)
-(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(eval-after-load "org"
+  '(require 'ox-md nil t))
