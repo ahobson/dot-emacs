e53c95607de766ed542dfc3d2dfc145aeb711068
Andrew Hobson <ahobson@cybraics.com>
2018-06-12 11:17:48 -0400

    Update package list in custom

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yasnippet magit-popup git-commit company yaml-mode smex smartparens scss-mode rainbow-mode pyvenv puppet-mode paredit markdown-mode magit lua-mode jedi ido-completing-read+ idle-highlight-mode ibuffer-vc go-mode flx-ido ensime enh-ruby-mode edit-server dockerfile-mode clojure-mode autopair))))
+    (yasnippet magit-popup git-commit ido-completing-read+ company clojure-mode yaml-mode smex smartparens scss-mode scala-mode2 rainbow-mode pyvenv puppet-mode paredit markdown-mode magit lua-mode jedi ido-ubiquitous idle-highlight-mode ibuffer-vc go-mode flx-ido ensime enh-ruby-mode edit-server dockerfile-mode cider autopair))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
