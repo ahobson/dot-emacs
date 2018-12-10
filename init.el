672c1ee61aa6b5051921075375956393183d28fa
Andrew Hobson <ahobson@cybraics.com>
2018-12-10 10:17:17 -0500

    Add missing packages

--- init.el
+++ init.el
@@ -46,6 +46,7 @@
 (setq el-get-user-package-directory "~/.emacs.d/el-get-user-package")
 
 ;; clojure editing
+(package-install 'cider)
 (package-install 'paredit)
 (package-install 'clojure-mode)
 (package-install 'company)
@@ -55,6 +56,7 @@
 ;; ;(el-get-bundle elpa:scala-mode2)
 ;; (el-get-bundle elpa:ensime)
 (package-install 'ensime)
+(package-install 'scala-mode2)
 (setq ensime-default-java-flags
       (list
        "-Xss2m" "-Xms4G" "-Xmx4G"
@@ -70,6 +72,7 @@
 ;; useful navigation packages
 (package-install 'ido-completing-read+)
 (package-install 'flx-ido)
+(package-install 'ido-ubiquitous)
 (flx-ido-mode t)
 ;; ido-mode is like magic pixie dust!
 (ido-mode t)
