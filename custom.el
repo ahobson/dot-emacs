6b7bd1744124c479788fe82f3813e2e2ce7fe117
Andrew Hobson <ahobson@cybraics.com>
2018-12-10 08:00:15 -0500

    Experiment with lsp

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (autopair cider clojure-mode company dockerfile-mode edit-server enh-ruby-mode ensime flx-ido git-commit go-mode ibuffer-vc idle-highlight-mode ido-completing-read+ jedi lua-mode magit magit-popup markdown-mode paredit puppet-mode pyvenv rainbow-mode scss-mode smartparens smex terraform-mode vue-mode yaml-mode yasnippet))))
+    (autopair cider clojure-mode company dash-functional dockerfile-mode edit-server ensime f flx-ido git-commit go-mode ht ibuffer-vc idle-highlight-mode ido-completing-read+ jedi lsp-mode lua-mode magit magit-popup markdown-mode paredit puppet-mode pyvenv rainbow-mode scss-mode smartparens smex spinner terraform-mode vue-mode yaml-mode yasnippet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
