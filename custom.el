daaf8fd03fcd0806f3ae44e359e767898b92fdc8
Andrew Hobson <ahobson@cybraics.com>
2018-12-17 11:30:49 -0500

    Sort modes

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/truss/org/agenda.org")))
  '(package-selected-packages
    (quote
-    (autopair cider-clojure company-completing dockerfile commit-company completing-dockerfile-edit+ ensime-flx git-go highlight-ibuffer idle ido ido-ido jedi-lua magit-magit markdown mode-mode mode mode-mode mode mode-mode mode mode-mode mode-mode2-paredit popup-puppet pyvenv-rainbow read-ruby scala scss-server-smartparens smex-ubiquitous vc-vue yaml yasnippet))))
+    (autopair cider clojure-mode company dash-functional dockerfile-mode edit-server ensime f flx-ido git-commit go-mode ht ibuffer-vc idle-highlight-mode ido-completing-read+ ido-ubiquitous jedi lsp-mode lua-mode magit magit-popup markdown-mode paredit puppet-mode pyvenv rainbow-mode scala-mode2 scss-mode smartparens smex spinner terraform-mode vue-mode yaml-mode yasnippet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
