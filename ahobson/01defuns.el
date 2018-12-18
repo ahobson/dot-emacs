05112039fb6ef14e59cadee72290227161cb82d9
Andrew Hobson <ahobson@gmail.com>
2018-12-18 09:01:28 -0500

    Cleanups for recent packages

--- ahobson/01defuns.el
+++ ahobson/01defuns.el
@@ -104,3 +104,14 @@
 
 (defun my-c-basic-offset ()
   (setq c-basic-offset 2))
+
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "[^[:space:]]+" "\\&" beg end))
