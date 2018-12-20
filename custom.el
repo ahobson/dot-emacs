97f0d33db61a8644a4768f237bbb696403cd34df
Andrew Hobson <ahobson@gmail.com>
2018-12-20 17:52:34 -0500

    Remove custom packages when using straight

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (autopair cider-clojure company-completing dockerfile commit-company completing-dockerfile-edit+ ensime-flx git-go highlight-ibuffer idle ido ido-ido jedi-lua magit-magit markdown mode-mode mode mode-mode mode mode-mode mode mode-mode mode-mode2-paredit popup-puppet pyvenv-rainbow read-ruby scala scss-server-smartparens smex-ubiquitous vc-vue yaml yasnippet))))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
