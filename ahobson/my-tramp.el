10d444aa45705622304dfe37e9b01427bcc608c8
Andrew Hobson <ahobson@truss.works>
2022-11-29 09:25:16 -0500

    More tramp exploring

--- ahobson/my-tramp.el
+++ ahobson/my-tramp.el
@@ -1,16 +1,67 @@
-;;; my-tramp.el --- Summary
+;;; my-tramp.el --- Summary                               -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
-;; (lsp-register-client
-;;  (make-lsp-client :new-connection (lsp-tramp-connection "/Users/ahobson/bin/tramp-gopls")
-;;                   :major-modes '(go-mode)
-;;                   :remote? t
-;;                   :server-id 'gopls-remote))
+
+;; from https://github.com/emacs-lsp/lsp-mode/issues/2709#issuecomment-860753973
+
+(defun lsp-tramp-connection-over-ssh-port-forwarding (command &optional test-command)
+  "Like lsp-tcp-connection, but uses SSH portforwarding."
+  (cl-check-type command (or string
+                             function
+                             (and list
+                                  (satisfies (lambda (l)
+                                               (seq-every-p (lambda (el)
+                                                              (stringp el))
+                                                            l))))))
+  (list
+   :connect (lambda (filter sentinel name environment-fn _workspace)
+              (let* ((host "localhost")
+                     (lsp-port (lsp--find-available-port host (cl-incf lsp--tcp-port)))
+                     (command (with-parsed-tramp-file-name buffer-file-name nil
+                                (lsp-message "[tcp/ssh hack] running LSP %s on %s / %s" command host localname)
+                                (let* ((unix-socket (format "/tmp/lsp-ssh-portforward-%s.sock" lsp-port))
+                                       (command (list
+                                                 "ssh"
+                                                 ;; "-vvv"
+                                                 "-L" (format "%s:%s" lsp-port unix-socket)
+                                                 host
+                                                 "socat"
+                                                 (format "unix-listen:%s" unix-socket)
+                                                 (format "system:'\"cd %s && %s\"'" (file-name-directory localname) command)
+                                                 )))
+                                  (lsp-message "using local command %s" command)
+                               command)))
+                     (final-command (if (consp command) command (list command)))
+                     (_ (unless (executable-find (cl-first final-command))
+                          (user-error (format "Couldn't find executable %s" (cl-first final-command)))))
+                     (process-environment
+                      (lsp--compute-process-environment environment-fn))
+                     (proc (make-process :name name :connection-type 'pipe :coding 'no-conversion
+                                         :command final-command :sentinel sentinel :stderr (format "*%s::stderr*" name) :noquery t))
+                     (tcp-proc (progn
+                                 (sleep-for 1) ; prevent a connection before SSH has run socat. Ugh.
+                                 (lsp--open-network-stream host lsp-port (concat name "::tcp")))))
+
+                ;; TODO: Same :noquery issue (see above)
+                (set-process-query-on-exit-flag proc nil)
+                (set-process-query-on-exit-flag tcp-proc nil)
+                (set-process-filter tcp-proc filter)
+                (cons tcp-proc proc)))
+   :test? (lambda () t)))
+
+
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-tramp-connection-over-ssh-port-forwarding "tramp-gopls")
+                  :major-modes '(go-mode)
+                  :remote? t
+                  :server-id 'gopls-remote))
 
 (with-eval-after-load "tramp"
   (add-to-list 'tramp-remote-path (expand-file-name "~/.nix-profile/bin"))
   (add-to-list 'tramp-remote-path (expand-file-name "~/bin")))
+
+
 
 ;;(customize-set-variable 'tramp-encoding-shell "/bin/zsh")
 
