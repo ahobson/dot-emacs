5e3abcb404c626ac01ced9b9c2292e17bb30223d
Andrew Hobson <ahobson@gmail.com>
2023-09-18 13:01:59 -0400

    Update safe themes

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7" default)))
+   '("7dc296b80df1b29bfc4062d1a66ee91efb462d6a7a934955e94e786394d80b71" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
